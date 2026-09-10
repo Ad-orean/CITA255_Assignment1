7a039deb6a5c313ae00e5fb1721468b436f47ea3
Ad-orean <Evans000@morrisville.edu>
2026-09-09 22:51:42 -0400

    Add project files.

---
+++ Assignment/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace Assignment;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    void OnCalculateClicked(object sender, EventArgs e)
+    {
+        double bill = double.Parse(billEntry.Text);
+
+        double tip = bill * 0.20;
+        double total = bill + tip;
+
+        resultLabel.Text = $"Tip: {tip:C}\nTotal: {total:C}";
+    }
+}
